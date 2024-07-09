5eff099c240dfe6b2478bbf26f8e03998530470f
theroflx <gabellaoff@yandex.ru>
2024-07-09 16:18:03 +0500

    Тест айпи

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -4,7 +4,7 @@ const config = {
   siteName: packageJson.name,
   imageApiUrl: import.meta.env.VITE_BASE_URL,
   serverUrl: {
-    main: import.meta.env.VITE_BASE_URL,
+    main: '95.163.221.120:9000',
     wsMain: import.meta.env.VITE_BASE_WS_URL,
   },
   serverPlacementsUrl: `${import.meta.env.VITE_BASE_URL}/lk_api/files/placements/`,
